0f974d6246530b11cac566bbfb47c08f5dd167d4
Brandon Dail <Kierkegaurd@gmail.com>
2016-05-10 18:04:52 -0500

    Passing test for issue 375

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
-# enzyme-test-repo
+# enzyme-test-repo (issue #375)
 
-This is a bare-bones repo used to reproduce errors in enzyme. You can fork this repo to provide a simple repo reproducing an issue with enzyme.
-
-It contains various scripts for installing the latest versions of React, enzyme, and jsdom (see the package.json file for specifics).
+https://github.com/airbnb/enzyme/issues/375
--- test.js
+++ test.js
@@ -5,12 +5,18 @@ import {mount} from 'enzyme';
 
 chai.use(chaiEnzyme());
 
-class TestComponent extends React.Component {
+class TestSVGComponent extends React.Component {
   render() {
     return (
-      <div>
-        <h1>Hello, {this.props.name}</h1>
-      </div>
+      <svg className='test' height="100" width="100">
+        <circle
+          cx="50"
+          cy="50"
+          r="40"
+          stroke="black"
+          strokeWidth="3"
+          fill="red" />
+      </svg>
     );
   }
 }
@@ -20,9 +26,9 @@ class TestComponent extends React.Component {
  * the test that you are trying to reproduce.
  */
 
-describe('AN EXAMPLE TEST SUITE', () => {
-  it('ENTER YOUR DESCRIPTION HERE', () => {
-    const wrapper = mount(<TestComponent/>);
-    expect(true).to.equal(true);
+describe('mount() and SVG support', () => {
+  it('should call methods that use findDOMNode on SVGs without an error', () => {
+    const wrapper = mount(<TestSVGComponent/>);
+    expect(wrapper.find('.test')).to.have.length(1);
   });
 })
