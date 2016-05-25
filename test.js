1e902877581a8724f88fe98ccbeeaa793ff0cde0
Brandon Dail <Kierkegaurd@gmail.com>
2016-05-25 11:25:38 -0500

    Failing test case for issue #413

--- test.js
+++ test.js
@@ -5,11 +5,28 @@ import {mount} from 'enzyme';
 
 chai.use(chaiEnzyme());
 
+const firstClass = 'foo'
+const secondClass = 'bar'
+
+const transformedFirstClass = firstClass + '-'
+const transformedSecondClass = secondClass
+
+const controlClassName = `${firstClass} ${secondClass}`
+const controlQuery = `.${firstClass}.${secondClass}`
+
+const testClassName = `${transformedFirstClass} ${transformedSecondClass}`;
+const testQuery = `.${transformedFirstClass}.${transformedSecondClass}`;
+
 class TestComponent extends React.Component {
   render() {
     return (
       <div>
-        <h1>Hello, {this.props.name}</h1>
+        <div className={testClassName}>
+          <h3>Testing</h3>
+        </div>
+        <div className={controlClassName}>
+          <h3>Another Testing</h3>
+        </div>
       </div>
     );
   }
@@ -20,9 +37,21 @@ class TestComponent extends React.Component {
  * the test that you are trying to reproduce.
  */
 
-describe('AN EXAMPLE TEST SUITE', () => {
-  it('ENTER YOUR DESCRIPTION HERE', () => {
-    const wrapper = mount(<TestComponent/>);
-    expect(true).to.equal(true);
+describe('Issue 413', () => {
+
+  before(() => {
+    console.log(`The control class name is ${controlClassName}`);
+    console.log(`The test class name is ${testClassName}`);
+    console.log(`The control query is ${controlClassName}`);
+    console.log(`The test query is ${testQuery}`);
+  });
+
+  it('should find elements via .find() with alphabetical class names', () => {
+    const wrapper = mount(<TestComponent />);
+    expect(wrapper.find(controlQuery)).to.have.length(1);
+  })
+  it('It should find elements with transformed class names', () => {
+    const wrapper = mount(<TestComponent />);
+    expect(wrapper.find(testQuery)).to.have.length(1);
   });
 })
